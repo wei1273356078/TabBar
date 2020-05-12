2ebeb1d24dd74774a093d5a8f994b9c49831bae7
unknown <1273356078@qq.com>
2020-05-12 15:21:28 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <main-tab-bar></main-tab-bar>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import MainTabBar from 'components/MainTabBar'
+export default {
+  name: 'App',
+  components: {
+    MainTabBar
+  }
+}
+</script>
+
+<style>
+/* 在style标签中导入,要加@ */
+@import 'assets/css/base';
+
+</style>
